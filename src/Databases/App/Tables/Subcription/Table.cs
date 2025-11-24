7368f684111d62d453be2775fee12c7e3a1e7edf
KhoaVD12 <duykhoa2kk@gmail.com>
2025-11-24 23:28:23 +0700

    add table

---
+++ src/Databases/App/Tables/Subcription/Table.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Iowa.Databases.App.Tables.Subcription;
+
+public class Table
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public Guid ProviderId { get; set; }
+    public Guid PackageId { get; set; }
+    public decimal Price { get; set; }
+    public decimal? DiscountedPrice { get; set; }
+    public string Currency { get; set; } = string.Empty;
+    public string ChartColor { get; set; } = string.Empty;
+    public Guid? DiscountId { get; set; }
+    public DateTime RenewalDate { get; set; }
+    public string? Status { get; set; } 
+    public DateTime CreatedDate { get; set; }
+    public Guid CreatedById { get; set; }
+    public DateTime? LastUpdated { get; set; }
+    public Guid? UpdatedById { get; set; }
+}
