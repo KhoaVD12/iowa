eea53d5d0223b12cc3b065a86201376d940d38b5
DaNX2 <nxd113322@gmail.com>
2025-11-26 21:12:24 +0700

    Add Payment History table

--- src/Databases/App/Tables/PaymentHistory/Table.cs
+++ src/Databases/App/Tables/PaymentHistory/Table.cs
@@ -2,5 +2,19 @@
 {
     public class Table
     {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid ProviderId { get; set; }
+        public Guid PackageId { get; set; }
+        public Guid? DiscountId { get; set; }
+        public string ChartColor { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public decimal? DiscountedPrice { get; set; }
+        public string Currency { get; set; } = string.Empty;
+        public DateTime PaymentDate { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? LastUpdate { get; set; }
+        public Guid CreateById { get; set; }
+        public Guid? UpdateById { get; set; }
     }
 }
