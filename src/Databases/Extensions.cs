589afc8ab3cbfa69a1d8e612cf4d84f081f37253
Vipertricks <hodactoan181905@gmail.com>
2025-12-02 11:19:44 +0700

    create extensions for AddDatabases

---
+++ src/Databases/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using Iowa.Databases.App;
+using Microsoft.EntityFrameworkCore;
+
+namespace Iowa.Databases
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddDatabases(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<IowaContext>(options =>
+                options.UseSqlServer("Server=localhost;Database=Iowa;Trusted_Connection=True;TrustServerCertificate=True"));
+            return services;
+        }
+    }
+}
--- src/Wolverine/Extensions.cs
+++ src/Wolverine/Extensions.cs
@@ -1,17 +1,11 @@
-﻿using Iowa.Databases.App;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Wolverine;
 
 namespace Iowa.Wolverine;
 
 public static class Extensions
 {
-    public static IServiceCollection AddDatabases(this IServiceCollection services, IConfiguration configuration)
-    {
-        services.AddDbContext<IowaContext>(options =>
-            options.UseSqlServer("Server=localhost;Database=Iowa;Trusted_Connection=True;TrustServerCertificate=True"));
-        return services;
-    }
+   
     public static IServiceCollection AddWolverines(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddWolverine(options =>
