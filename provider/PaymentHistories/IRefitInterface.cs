0f1eb5d40f30a54f658033078ddeade171c9fd7e
DaNX2 <nxd113322@gmail.com>
2025-12-08 20:50:17 +0700

    Provider - Payment history and add authorize in Controller Payment history.

---
+++ provider/PaymentHistories/IRefitInterface.cs
@@ -0,0 +1,20 @@
+﻿using Refit;
+namespace Provider.PaymentHistories;
+
+public interface IRefitInterface
+{
+    [Get("/api/payment-histories")]
+    Task<ApiResponse<Models.PaginationResults.Model<Model>>> Get([Query] Get.Parameters parameters);
+
+    [Post("/api/payment-histories")]
+    Task<ApiResponse<object>> Post([Body] Post.Payload payload);
+
+    [Put("/api/payment-histories")]
+    Task<ApiResponse<object>> Put([Body] Put.Payload payload);
+
+    [Delete("/api/payment-histories")]
+    Task<ApiResponse<object>> Delete([Query] Delete.Parameters parameters);
+
+    [Patch("/api/payment-histories")]
+    Task<ApiResponse<object>> Patch([Query] Patch.Parameters parameters, [Body] List<Patch.Operation> operations);
+}
---
+++ provider/PaymentHistories/Operations/Get/Parameters.cs
@@ -0,0 +1,23 @@
+﻿namespace Provider.PaymentHistories.Get;
+
+public class Parameters
+{
+    public Guid? Id { get; set; }
+    public Guid? ProviderId { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? IconUrl { get; set; }
+    public decimal? Price { get; set; }
+    public string? Currency { get; set; }
+    public string? Ids { get; set; }
+    public int? PageIndex { get; set; }
+    public int? PageSize { get; set; }
+    public string? SearchTerm { get; set; }
+    public string? Include { get; set; }
+    public string? SortBy { get; set; }
+    public string? SortOrder { get; set; }
+    public DateTime? CreatedDate { get; set; }
+    public DateTime? LastUpdated { get; set; }
+    public Guid? CreatedById { get; set; }
+    public Guid? UpdatedById { get; set; }
+}
